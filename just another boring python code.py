953cc2c3955d438c77eaa6ab8179754da777f7d6
tizianui <70412107+tizianui@users.noreply.github.com>
2020-09-09 10:00:28 -0700

    Add files via upload

---
+++ just another boring python code.py
@@ -0,0 +1,26 @@
+print("Am I a pedo ? Calculator")
+
+print(" ")
+print(" ")
+BlockingIOError
+age_of_me = int(input("What Is your age Potential Pedo"))
+if (age_of_me) >= 18:
+    age_of_me=1
+elif (age_of_me) < 18:
+    age_of_me=0
+
+age_of_love = int(input("What is the age of your love?"))
+if (age_of_love) >= 18 :
+    age_of_love=1
+elif (age_of_love) < 18 :
+    age_of_love=0
+
+if (age_of_me) == (age_of_love):
+    print("CONGRATS BRO YOU PASS THE PEDO TEST!!!!")
+else:
+    print(" juts remeber that age of consent is important so wait until shes 18+")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print("https://www.youtube.com/watch?v=9Deg7VrpHbM&ab_channel=VioletDirge")
